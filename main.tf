0ee32829a6ad1267df83de56e99813b0b609edd7
kavya <vijayakavya1226@gmail.com>
2023-12-30 23:51:56 -0800

    expense shell

--- main.tf
+++ main.tf
@@ -7,15 +7,15 @@ resource "aws_ssm_parameter" "params" {
 
 variable "parameters" {
   default = [
-    {name = "prod.rds.master_password", value = "expenseprod1234" type = "SecureString"},
-    {name= "prod.rds.master_username", value = "mysqlrdsadmin" type = "String"},
-    {name= "prod.expense.frontend.backend_url", value = "http://backend-dev.kdevopsb26.online/" type = "String"},
-    {name= "rds.prod.endpoint", value = "prod-mysql.cluster-cbvsbeoyxek4.us-east-1.rds.amazonaws.com" type = "String"},
+    {name = "prod.rds.master_password", value = "expenseprod1234", type = "SecureString"},
+    {name= "prod.rds.master_username", value = "mysqlrdsadmin", type = "String"},
+    {name= "prod.expense.frontend.backend_url", value = "http://backend-dev.kdevopsb26.online/", type = "String"},
+    {name= "rds.prod.endpoint", value = "prod-mysql.cluster-cbvsbeoyxek4.us-east-1.rds.amazonaws.com", type = "String"},
 
     ## Dev env
-    {name= "dev.expense.frontend.backend_url", value = "http://backend-dev.kdevopsb26.online/" type = "String"},
+    {name= "dev.expense.frontend.backend_url", value = "http://backend-dev.kdevopsb26.online/", type = "String"},
 
     ## Common env
-    {name= "jenkins_password", value = "admin123" type = "String"}
+    {name= "jenkins_password", value = "admin123", type = "String"}
   ]
 }
